a4fee1ffba0affea355e412d899efcd2674029b4
Luis Carlos A. Rojas Torres <redtower.soft@gmail.com>
2022-05-28 00:41:05 -0300

    GUI Structure added
    - Added ComboBoxes to select notes and scales
    - Added QListWidget to contain chords per note, a label indicates which note...
    - GraphicView added to plot the fretboard
    
    No functionality implemented only GUI!

--- QtGuitar/MainWindow.cpp
+++ QtGuitar/MainWindow.cpp
@@ -8,6 +8,16 @@ MainWindow::MainWindow(QString version, QWidget *parent)
 {
     ui->setupUi(this);
     setWindowTitle(QString("QtGuitar " + version));
+
+    ui->comboBox_notes->addItem(QString("C"));
+    ui->comboBox_notes->addItem(QString("D"));
+    ui->comboBox_notes->addItem(QString("E"));
+    ui->comboBox_notes->addItem(QString("F"));
+    ui->comboBox_notes->addItem(QString("G"));
+    ui->comboBox_notes->addItem(QString("A"));
+    ui->comboBox_notes->addItem(QString("B"));
+
+    ui->comboBox_scales->addItem(QString("Major"));
 }
 
 MainWindow::~MainWindow()
